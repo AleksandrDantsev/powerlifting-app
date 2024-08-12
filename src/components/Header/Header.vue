7772de7e1bdd2e7f0fbd718cd93da32d170709b0
Alexander Dantsev <ivan_sidorov_99@internet.ru>
2024-08-12 15:30:11 +0300

    create template Header

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <header class="header">
+        <div class="header-wrapper">
+            <div class="header-left-interraction">
+                <div class="header-arrow-back"></div>
+                <div class="header-name-page"></div>
+            </div>
+            <div class="header-burger"></div>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+    .header {
+        height: 50px;
+        background-color: #fff;
+        box-shadow: 0 2px 10px 0 rgb(226, 226, 226);
+        margin-bottom: 25px;
+    }
+</style>
